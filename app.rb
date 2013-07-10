1a779ddd9a0f3b216604825d5d3a2edd29257857
Alex Mekhovov <alex.mehovov@gmail.com>
2013-07-10 20:40:31 +0300

    add: meters to feet convertion

--- app.rb
+++ app.rb
@@ -26,3 +26,19 @@ get '/fahrenheit_to_celsius' do
   }.to_json
 end
 
+
+# Convert Meters to Feet
+get '/meters_to_feet' do
+  meters = Convertor.new(params[:value].to_f, MetersToFeetConvertor.new)
+  { :meters => params[:value].to_f,
+    :feet => meters.converted_value.round(2)
+  }.to_json
+end
+
+# Convert Feet to Meters
+get '/feet_to_meters' do
+  feet = Convertor.new(params[:value].to_f, MetersToFeetConvertor.new(true))
+  { :meters => feet.converted_value.round(2),
+    :feet => params[:value].to_f
+  }.to_json
+end
--- public/js/app.js
+++ public/js/app.js
@@ -13,11 +13,10 @@ function render_celcius_slider(value) {
               $("#f-amount").html(res.fahrenheit);
               render_fahrenheit_slider(res.fahrenheit);
             });
-            
         }
     });
 }
-
+// Fahrenheit slider
 function render_fahrenheit_slider(value) {
     $('#f-slider').slider({
         range: "min",
@@ -31,7 +30,40 @@ function render_fahrenheit_slider(value) {
               $("#c-amount").html(res.celsius);
               render_celcius_slider(res.celsius);
             });
-            
+        }
+    });
+}
+// Meters slider
+function render_meters_slider(value) {
+    $('#m-slider').slider({
+        range: "min",
+        min: 0,
+        max: 97,
+        value: value,
+        slide: function (event, ui) {
+            $.get('/meters_to_feet?value=' + ui.value, function(data) {
+              res = $.parseJSON(data);
+              $("#m-amount").html(ui.value);
+              $("#ft-amount").html(res.feet);
+              render_feet_slider(res.feet);
+            });
+        }
+    });
+}
+// Feet slider
+function render_feet_slider(value) {
+    $('#ft-slider').slider({
+        range: "min",
+        min: 0,
+        max: 320,
+        value: value,
+        slide: function (event, ui) {
+            $.get('/feet_to_meters?value=' + ui.value, function(data) {
+              res = $.parseJSON(data);
+              $("#ft-amount").html(ui.value);
+              $("#m-amount").html(res.meters);
+              render_meters_slider(res.meters);
+            });
         }
     });
 }
@@ -39,8 +71,12 @@ $(function () {
 
     render_celcius_slider(20);
     render_fahrenheit_slider(68);
-
     $("#c-amount").html($("#c-slider").slider("value"));
     $("#f-amount").html($("#f-slider").slider("value"));
+
+    render_meters_slider(10);
+    render_feet_slider(32.8);
+    $("#m-amount").html($("#m-slider").slider("value"));
+    $("#ft-amount").html($("#ft-slider").slider("value"));
 
 });
